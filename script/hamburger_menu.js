c209b2100f85ce5196102bf688c18b05d3f4628a
Yusuke0620 <boston19851229@gmail.com>
2024-02-19 23:44:57 +0900

    本日最終追加

--- script/hamburger_menu.js
+++ script/hamburger_menu.js
@@ -8,31 +8,31 @@ HTMLの初期文書が読み込まれたら
 定数bodyにbodyを代入
 */
 document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector(".hamburger_menu");
-    const body = document.querySelector("body");  
-  
-    /* 
-    ハンバーガーメニューがクリックされたら
-    定数menuにactiveセレクタを代入
-    定数bodyにbackcolorセレクタを代入
-    */
+  const menu = document.querySelector(".hamburger_menu");
+  const body = document.querySelector("body");  
+
+  /* 
+  ハンバーガーメニューがクリックされたら
+  定数menuにactiveセレクタを代入
+  定数bodyにback_colorセレクタを代入
+  */
     menu.addEventListener("click", () => {
-      menu.classList.toggle("active"); // メニューアイコンの形状変更
-      body.classList.toggle("backcolor"); //背景色変更
-  
+    menu.classList.toggle("active"); // メニューアイコンの形状変更
+    body.classList.toggle("back_color"); //背景色変更
+
+    /* 
+    spanは複数あるためNodeListオブジェクトに対しforEachを使用する
+    span要素の集合に対して繰り返し処理を行う
+    */
+    const menuSpan = document.querySelectorAll(".hamburger_menu span");
+    menuSpan.forEach(span => {
+
       /* 
-      spanは複数あるためNodeListオブジェクトに対しforEachを使用する
-      span要素の集合に対して繰り返し処理を行う
-      */
-      const menuSpan = document.querySelectorAll(".hamburger_menu span");
-      menuSpan.forEach(span => {
-  
-        /* 
-        メニューがクリックされclassListが追加されたら
-        #fff(true)、削除されたら#333(false)
-        メニューバーの色を切り替える
-        */     
-        span.style.backgroundColor = menu.classList.contains("active") ? "#fff" : "#333333";
-      });
+      メニューがクリックされclassListが追加されたら
+      #fff(true)、削除されたら#333(false)
+      メニューバーの色を切り替える
+      */     
+      span.style.backgroundColor = menu.classList.contains("active") ? "#fff" : "#333333";
     });
-  })
+  });
+})
